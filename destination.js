80de92f7cf41cfbab3d93b2ca45a21bec896f7c1
Neve-06 <6255234@rts-365.dk>
2025-02-07 10:50:06 +0100

    fetched id from json and all the info is in the log

--- destination.js
+++ destination.js
@@ -5,3 +5,7 @@ let params = new URLSearchParams(search)
 console.log(params);
 let id = params.get("id")
 console.log(id);
+
+fetch(`/data/${id}.json`)
+.then(response => response.json())
+.then(data => console.log(data))
