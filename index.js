88afea606ec101bb1b904b3c17eb7a78856c85cb
Neve-06 <6255234@rts-365.dk>
2025-02-18 12:39:35 +0100

    added classes for the figure tag and the button tag. also added click event for buttons

--- index.js
+++ index.js
@@ -6,12 +6,21 @@
         let sectionElm = document.createElement("section")
         sectionElm.classList.add("columns")
         sectionElm.innerHTML = data.destinations.map(destination => `
-            <figure>
+            <figure class="cardimg__container">
             <img src="/img/${destination.image}"></img>
-            <button>heart</button>
+            <button class="card__favoritebtn" data-favid="${destination.id}">heart</button>
             <a href="destination.html?id=${destination.id}">
             MORE
             </a>
-            </figure>` ).join("")
+            </figure>` )
+            .join("")
+
+            sectionElm.querySelectorAll(".card__favoritebtn").forEach(function(button){
+                button.addEventListener("click", function(event){
+                    console.log(event.target);
+                    
+            })
+        
+            })
     document.querySelector("#root").append(sectionElm)
     })
