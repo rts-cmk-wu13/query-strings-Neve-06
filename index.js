7b224f66f7495f76f703e9a94a68bc41bc5fb620
Neve-06 <6255234@rts-365.dk>
2025-02-18 11:12:58 +0100

    added figure to section elm

--- index.js
+++ index.js
@@ -6,9 +6,12 @@
         let sectionElm = document.createElement("section")
         sectionElm.classList.add("columns")
         sectionElm.innerHTML = data.destinations.map(destination => `
+            <figure>
             <img src="/img/${destination.image}"></img>
+            <button>heart</button>
             <a href="destination.html?id=${destination.id}">
             MORE
-            </a>` ).join("")
+            </a>
+            </figure>` ).join("")
     document.querySelector("#root").append(sectionElm)
     })
