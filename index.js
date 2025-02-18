bfa0b607d36c844e758d469f6fabe943be1a0e3b
Neve-06 <6255234@rts-365.dk>
2025-02-18 09:05:34 +0100

    *small edit, wrote MORE instead of destination.title and added a classlist to the section

--- index.js
+++ index.js
@@ -4,10 +4,11 @@
     .then(response => response.json())
     .then(data => {
         let sectionElm = document.createElement("section")
+        sectionElm.classList.add("columns")
         sectionElm.innerHTML = data.destinations.map(destination => `
             <img src="/img/${destination.image}"></img>
             <a href="destination.html?id=${destination.id}">
-            ${destination.title}
+            MORE
             </a>` ).join("")
     document.querySelector("#root").append(sectionElm)
     })
