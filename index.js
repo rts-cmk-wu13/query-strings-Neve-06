6101153e1e8cfcc132df394b3f66f0bdc787a757
Neve-06 <6255234@rts-365.dk>
2025-02-19 09:38:10 +0100

    added classlist remove/add to the styling of the favorite button. added read from local storage in the favorites variable

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-let favorites = []
+let favorites = readFromLocalStorage("favorites") || []
+console.log(favorites);
+
+// !favorites && favorites = []
+
 
     fetch("/data/destinations.json")
     .then(response => response.json())
@@ -6,9 +10,9 @@ let favorites = []
         let sectionElm = document.createElement("section")
         sectionElm.classList.add("columns")
         sectionElm.innerHTML = data.destinations.map(destination => `
-            <figure class="cardimg__container">
+            <figure class="card">
             <img src="/img/${destination.image}"></img>
-            <button class="card__favoritebtn" data-favid="${destination.id}">heart</button>
+            <button class="card__favoritebtn ${favorites.includes(destination.id.toString())?"card__favoritebtn--selected" : ""}" data-favid="${destination.id}">heart</button>
             <a href="destination.html?id=${destination.id}">
             MORE
             </a>
@@ -21,14 +25,16 @@ let favorites = []
                     if (favorites.includes(currentId)){
                         let newFavorites = favorites.filter(id => id != currentId)
                         favorites = newFavorites
+                        event.target.classList.remove("card__favoritebtn--selected")
                         console.log(favorites);
                         
                     }else {
                         favorites.push(currentId)
+                        event.target.classList.add("card__favoritebtn--selected")
                         console.log(favorites);
                         
                     }
-                    
+                    saveToLocalStorage("favorites", favorites)
             })
         
             })
