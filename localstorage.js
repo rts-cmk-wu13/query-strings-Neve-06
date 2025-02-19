a70d47fd1d0b84b13217bfe6b5c23e49d4371778
Neve-06 <6255234@rts-365.dk>
2025-02-19 10:00:54 +0100

    added params to localstorage

--- localstorage.js
+++ localstorage.js
@@ -1,8 +1,28 @@
+/**
+ * 
+ * @typedef {string | number | boolean | object | any[]} localStorageData - value to be saved in local storage
+ */
+
+
+
+/**
+ * Save an item to local storage
+ * @param {string} key - key to be used in local storage
+ * @param {localStorageData} value
+ * @returns {string}
+ */
+
+
 function saveToLocalStorage(key, value){
     localStorage.setItem(key, JSON.stringify(value))
     return "Data was saved with the key " + key
 }
 
+/**
+ * 
+ * @param {string} key - key to be read from local storage 
+ * @returns {string | number | boolean | object | any[]}
+ */
 
 function readFromLocalStorage(key){
     return JSON.parse(localStorage.getItem(key))
@@ -13,3 +33,6 @@ function deleteFromLocalStorage(key){
     localStorage.removeItem(key)
     return "The element with key " + key + " was deleted";
 }
+
+
+saveToLocalStorage()
